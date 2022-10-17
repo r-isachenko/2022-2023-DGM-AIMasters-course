cc70d87c4adaced749e7e0575a5fc37878fbbdb5
Petr Mokrov <mokrov.pv@phystech.edu>
2022-10-17 18:48:59 +0000

    update seminar6

--- seminars/seminar6/seminar6_utils.py
+++ seminars/seminar6/seminar6_utils.py
@@ -110,4 +110,87 @@ def manual_zero_grad(*tensors):
         if tsr.grad is not None:
             tsr.grad.detach_()
             tsr.grad.zero_()
+
+class DataLoaderWrapper:
+    '''
+    Helpful class for using the 
+    torch.distribution in torch's 
+    DataLoader manner
+    '''
     
+    class DatasetEmulator:
+        
+        def __init__(self, len_):
+            self.len_ = len_
+            pass
+        
+        def __len__(self):
+            return self.len_
+
+    class FiniteRepeatDSIterator:
+
+        def __init__(self, sampler, batch_size, n_batches):
+            dataset = sampler.sample((batch_size * n_batches,))
+            assert(len(dataset.shape) >= 2)
+            new_size = (n_batches, batch_size) + dataset.shape[1:]
+            self.dataset = dataset.view(new_size)
+            self.batch_size = batch_size
+            self.n_batches = n_batches
+        
+        def __iter__(self):
+            for i in range(self.n_batches):
+                yield self.dataset[i]
+    
+    class FiniteUpdDSIterator:
+
+        def __init__(self, sampler, batch_size, n_batches):
+            self.sampler = sampler
+            self.batch_size = batch_size
+            self.n_batches = n_batches
+        
+        def __iter__(self):
+            for i in range(self.n_batches):
+                yield self.sampler.sample((self.batch_size,))
+            
+    class InfiniteDsIterator:
+
+        def __init__(self, sampler, batch_size):
+            self.sampler = sampler
+            self.batch_size = batch_size
+        
+        def __iter__(self):
+            return self
+        
+        def __next__(self):
+            return self.sampler.sample((self.batch_size,))
+
+
+    def __init__(self, sampler, batch_size, n_batches=None, store_dataset=False):
+        '''
+        n_batches : count of batches before stop_iterations, if None, the dataset is infinite
+        store_datset : if n_batches is not None and store_dataset is True, 
+        during the first passage through the dataset the data will be stored,
+        and all other epochs will use the same dataset, stored during the first pass
+        '''
+        self.batch_size = batch_size
+        self.n_batches = n_batches
+        self.store_dataset = store_dataset
+        self.sampler = sampler
+
+        if self.n_batches is None:
+            self.ds_iter = DataLoaderWrapper.InfiniteDsIterator(
+                sampler, self.batch_size)
+            return
+        
+        self.dataset = DataLoaderWrapper.DatasetEmulator(self.batch_size * self.n_batches)
+        if not self.store_dataset:
+            self.ds_iter = DataLoaderWrapper.FiniteUpdDSIterator(
+                sampler, self.batch_size, self.n_batches)
+            return
+        
+        self.ds_iter = DataLoaderWrapper.FiniteRepeatDSIterator(
+            sampler, self.batch_size, self.n_batches)
+
+    
+    def __iter__(self):
+        return iter(self.ds_iter)
