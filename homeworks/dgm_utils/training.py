a782b7566d2d096e716adfc6042c36bbaa12a5e5
Roman Vlasov <vlasov.ri@phystech.edu>
2022-12-26 20:36:33 +0300

    add_np

--- homeworks/dgm_utils/training.py
+++ homeworks/dgm_utils/training.py
@@ -3,8 +3,8 @@ from .visualize import init_samples_vis_ctx, show_epoch_samples
 from collections import defaultdict
 from tqdm.notebook import tqdm
 
-import np
 import torch
+import numpy as np
 from torch import optim
 
 
